2be97ddaed46f983a06d592c4b04f47014420821
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-06-06 12:35:30 +0100

    input removed

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,106 +1,120 @@
 import {
-    faClapperboard,
-    faMagnifyingGlass,
-  } from "@fortawesome/free-solid-svg-icons";
-  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-  import React, { useState } from "react";
-  import Switch from "../components/Switch";
-  import Modal from "../components/Modal";
-  
-  const Browse = () => {
-    const [showModal, setShowModal] = useState(false);
-  
-  
-  
-    
-    return (
-      <div>
-        <section className="bg-orange">
-          <nav className="bg-orange">
-            <a href="/">
-              <div className="logo-img-browse bg-orange ">
-                <FontAwesomeIcon
-                  className="icon-browse bg-orange"
-                  icon={faClapperboard}
-                />
-                <h2 className="logo-browse bg-orange">movies</h2>
-              </div>
-            </a>
-            <ul>
-              <li className="lists-browse bg-orange">
-                <a className="bg-orange-list" href="/">
-                  {" "}
-                  Home{" "}
-                </a>
-              </li>
-              <li className="lists-browse bg-orange">
-                <a className="bg-orange-list" href="browse">
-                  Browse Movies
-                </a>
-              </li>
-              <li className="lists-browse bg-orange">
-                <a className="bg-orange-list" href="favourites">
-                  Favourites
-                </a>
-              </li>
-            </ul>
-          </nav>
-  
-          <h1 className="bm bg-orange">Browse Our Movies</h1>
-  
-          <div className="search-container-browse bg-orange">
-            <form action="" className="bg-orange">
-              <input
-                className="browse-input"
-                type="text"
-                placeholder="Search thousands of movies..."
-                name="Search"
+  faClapperboard,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Switch from "../components/Switch";
+import Modal from "../components/Modal";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Browse = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  async function fetchUsers() {
+    const { data } = await axios.get(
+      "https://www.omdbapi.com/?i=tt3896198&apikey=8e3ddd4c&s=fast"
+    );
+    setUsers(data.Search);
+    console.log(data);
+  }
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <section className="bg-orange">
+        <nav className="bg-orange">
+          <a href="/">
+            <div className="logo-img-browse bg-orange ">
+              <FontAwesomeIcon
+                className="icon-browse bg-orange"
+                icon={faClapperboard}
               />
-              <button className="button-browse">
-                <FontAwesomeIcon
-                  className="magnify-browse"
-                  icon={faMagnifyingGlass}
-                />
-              </button>
-            </form>
-          </div>
-        </section>
-  
-        <section className="btm-half">
-          <h2 className="search-results">Search results</h2>
-          <h2 className="filter">Filter by year</h2>
-         <button className="switch-btn"   onClick={()=>setShowModal(!showModal)}> <Switch/> </button> 
-         
-        </section>
-  
-        <div className="row">
-          <div className="user-list">
-            <div className="user">
-              <div className="user-card">
-                <div className="user-card__container">
-                  <h3>
-                    Poster: <b>Poster</b>{" "}
-                  </h3>
-                  <p>
-                    Title: <b>Title</b>{" "}
-                  </p>
-                  <p>
-                    Type: <b>Type</b>
-                  </p>
-                  <p>
-                    Year: <b>Year</b>
-                  </p>
+              <h2 className="logo-browse bg-orange">movies</h2>
+            </div>
+          </a>
+          <ul>
+            <li className="lists-browse bg-orange">
+              <a className="bg-orange-list" href="/">
+                {" "}
+                Home{" "}
+              </a>
+            </li>
+            <li className="lists-browse bg-orange">
+              <a className="bg-orange-list" href="browse">
+                Browse Movies
+              </a>
+            </li>
+            <li className="lists-browse bg-orange">
+              <a className="bg-orange-list" href="favourites">
+                Favourites
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <h1 className="bm bg-orange">Browse Our Movies</h1>
+
+        <div className="search-container-browse bg-orange">
+          <form action="" className="bg-orange">
+            <input onChange={(event) => {
+                console.log(event.target.value)
+            }}
+              className="browse-input"
+              type="text"
+              placeholder="Search thousands of movies..."
+              name="Search"
+            />
+            <button className="button-browse">
+              <FontAwesomeIcon
+                className="magnify-browse"
+                icon={faMagnifyingGlass}
+              />
+            </button>
+          </form>
+        </div>
+      </section>
+
+      <section className="btm-half">
+        <h2 className="search-results">Search results</h2>
+        <h2 className="filter">Filter by year</h2>
+        <button className="switch-btn" onClick={() => setShowModal(!showModal)}>
+          {" "}
+          <Switch />{" "}
+        </button>
+      </section>
+
+      {showModal ? <Modal /> : null}
+
+      {users.map((user) => {
+        return (
+          <div className="row">
+            <div className="user-list">
+              <div className="user">
+                <div className="user-card">
+                  <div className="user-card__container">
+                    <img className="images" src={user.Poster} alt="" />{" "}
+                    <p>
+                      Title: <b>{user.Title}</b>{" "}
+                    </p>
+                    <p>
+                      Type: <b>{user.Type}</b>
+                    </p>
+                    <p>
+                      Year: <b>{user.Year}</b>
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-  
-        {showModal ? <Modal /> : null}
-      
-      </div>
-    );
-  };
-  
-  export default Browse;
-  
+        );
+      })}
+    </div>
+  );
+};
+
+export default Browse;
